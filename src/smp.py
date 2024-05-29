837f4711e8e14246165316bcfef3e31916386c52
linweizhe <linweizhe94@qq.com>
2024-05-30 02:37:26 +0800

    first

--- src/smp.py
+++ src/smp.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 import segmentation_models_pytorch as smp
 import torch
+from torchvision.transforms import transforms
 
 device = (
     "cuda"
@@ -12,19 +13,19 @@ device = (
 )
 print(f"Using {device} device")
 
-aux_params = dict(
-    pooling='avg',  # one of 'avg', 'max'
-    dropout=0.5,  # dropout ratio, default is None
-    activation='sigmoid',  # activation function, default is None
-    classes=4,  # define number of output labels
-)
+# aux_params = dict(
+#     pooling='avg',  # one of 'avg', 'max'
+#     dropout=0.5,  # dropout ratio, default is None
+#     activation='sigmoid',  # activation function, default is None
+#     classes=4,  # define number of output labels
+# )
 
 model = smp.Unet(
     encoder_name="resnet152",  # choose encoder, e.g. mobilenet_v2 or efficientnet-b7
     encoder_weights="imagenet",  # use `imagenet` pre-trained weights for encoder initialization
     in_channels=3,  # model input channels (1 for gray-scale images, 3 for RGB, etc.)
     # classes=3,  # model output channels (number of classes in your dataset)
-    aux_params=aux_params
+    # aux_params=aux_params
 )
 
 model.to(device)
@@ -36,30 +37,33 @@ image = cv2.imread("test.png")
 # image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 print(image.shape)
 image = cv2.resize(image, (416, 256))
-cv2.imshow("image", image)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-image = np.transpose(image, (2, 0, 1))
-print(image.shape)
-image = np.expand_dims(image, axis=0)
-print(image.shape)
-image = torch.from_numpy(image).float()
-print(image.shape)
+# cv2.imshow("image", image)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+ndarray = np.transpose(image, (2, 0, 1))
+tensor = torch.from_numpy(ndarray).unsqueeze(0).float().to(device)
+print(f"Shape of tensor: {tensor.shape}")
 
-with torch.no_grad():  # no need to calculate gradients during evaluation
-    image = image.to(device)
-    segmented_image, x = model(image)
+# masks, labels = model.predict(tensor)
+masks = model.predict(tensor)
+print(f"Shape of masks: {masks.shape}")
 
 # Convert the output tensor to a numpy array
-segmented_image = segmented_image.cpu().numpy()
+masks = transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])(masks)
+masks = masks.cpu().numpy()
 
 # Perform an argmax operation to get the most probable class for each pixel
-segmented_image = segmented_image[0][0]
+mask = masks[0][0]
 
 # Normalize the segmented image to range 0-255
-segmented_image = (segmented_image / np.max(segmented_image) * 255).astype(np.uint8)
+# segmented_image = (mask / np.max(mask) * 255).astype(np.uint8)
+# cv2.imshow('Segmented Image', segmented_image)
 
-# Display the segmented image
-cv2.imshow('Segmented Image', segmented_image)
+mask = (mask * 255).astype(np.uint8)
+mask_3ch = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)
+cv2.imshow('Mask', mask_3ch)
+masked_image = cv2.bitwise_and(image, mask_3ch)
+cv2.imshow('Masked Image', masked_image)
+
 cv2.waitKey(0)
 cv2.destroyAllWindows()
