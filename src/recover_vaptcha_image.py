2c59d2b1da19271aa7df4af40e8724709c34cb3f
linweizhe <linweizhe94@qq.com>
2024-06-01 02:49:53 +0800

    update

--- src/recover_vaptcha_image.py
+++ src/recover_vaptcha_image.py
@@ -33,7 +33,7 @@ def find_split_count(gray: Mat):
     lines = cv2.HoughLinesP(dilation, 1, np.pi / 180, 100, minLineLength=100, maxLineGap=3)
     lines = np.squeeze(lines, axis=1)
     # 在原图上绘制线条
-    # line_background = np.zeros_like(gray)
+    # line_background = np.zeros((height, width, 3), dtype=np.uint8)
     # for line in lines:
     #     x1, y1, x2, y2 = line
     #     cv2.line(line_background, (x1, y1), (x2, y2), (0, 255, 0), 2)
@@ -85,37 +85,40 @@ def calculate_connection(connect_diff_matrix, max_link_count, max_link_size):
     return [to_list(node_dict[index]) for index, count in link_head_set.items() if count >= max_link_size]
 
 
-image = cv2.imread("1a5c9ecb5c54461daf1d18722c69ab4a.jpg")
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-piece_h_count, piece_w_count = find_split_count(gray.transpose()), find_split_count(gray)
-# piece_h_count, piece_w_count = 2, 5
-piece_h, piece_w = image.shape[0] // piece_h_count, image.shape[1] // piece_w_count
-image_pieces = [(image[y:y + piece_h, x:x + piece_w], gray[y:y + piece_h, x:x + piece_w])
-                for x in range(0, image.shape[1], piece_w)
-                for y in range(0, image.shape[0], piece_h)]
+def recover_image(src_image: Mat):
+    gray = cv2.cvtColor(src_image, cv2.COLOR_BGR2GRAY)
+    # piece_h_count, piece_w_count = find_split_count(gray.transpose()), find_split_count(gray)
+    piece_h_count, piece_w_count = 2, 5
+    piece_h, piece_w = src_image.shape[0] // piece_h_count, src_image.shape[1] // piece_w_count
+    image_pieces = [(src_image[y:y + piece_h, x:x + piece_w], gray[y:y + piece_h, x:x + piece_w])
+                    for x in range(0, src_image.shape[1], piece_w)
+                    for y in range(0, src_image.shape[0], piece_h)]
 
-# for (image_piece, gray_piece) in image_pieces:
-#     cv2.imshow("image_piece", image_piece)
-#     cv2.waitKey(0)
+    # for (image_piece, gray_piece) in image_pieces:
+    #     cv2.imshow("image_piece", image_piece)
+    #     cv2.waitKey(0)
 
-connect_diff_matrix = np.zeros((len(image_pieces), len(image_pieces), 2), dtype=np.int32)
-for i, (image_piece, gray_piece) in enumerate(image_pieces):
-    for j, (image_piece2, gray_piece2) in enumerate(image_pieces):
-        if i == j:
-            connect_diff_matrix[i, j] = np.iinfo(connect_diff_matrix.dtype).max
-            continue
-        rl_score = np.abs((gray_piece[:, -1] - gray_piece2[:, 0]).astype(np.int8)).sum()
-        bt_score = np.abs((gray_piece[-1, :] - gray_piece2[0, :]).astype(np.int8)).sum()
-        connect_diff_matrix[i, j] = rl_score, bt_score
-        print(f"i:{i}, j:{j}, rl_score: {rl_score}, bt_score: {bt_score}")
-rl_connect_diff_matrix, bt_connect_diff_matrix = connect_diff_matrix[:, :, 0], connect_diff_matrix[:, :, 1]
+    connect_diff_matrix = np.zeros((len(image_pieces), len(image_pieces), 2), dtype=np.int32)
+    for i, (image_piece, gray_piece) in enumerate(image_pieces):
+        for j, (image_piece2, gray_piece2) in enumerate(image_pieces):
+            if i == j:
+                connect_diff_matrix[i, j] = np.iinfo(connect_diff_matrix.dtype).max
+                continue
+            rl_score = np.abs((gray_piece[:, -1] - gray_piece2[:, 0]).astype(np.int8)).sum()
+            bt_score = np.abs((gray_piece[-1, :] - gray_piece2[0, :]).astype(np.int8)).sum()
+            connect_diff_matrix[i, j] = rl_score, bt_score
+    rl_connect_diff_matrix, bt_connect_diff_matrix = connect_diff_matrix[:, :, 0], connect_diff_matrix[:, :, 1]
 
-row_list = calculate_connection(rl_connect_diff_matrix, piece_h_count, piece_w_count)
-col_list = calculate_connection(bt_connect_diff_matrix, piece_w_count, piece_h_count)
+    row_list = calculate_connection(rl_connect_diff_matrix, piece_h_count, piece_w_count)
+    col_list = calculate_connection(bt_connect_diff_matrix, piece_w_count, piece_h_count)
 
-first_col = next(col for col in col_list if row_list[0][0] in col)
-row_list = sorted(row_list, key=lambda row: first_col.index(row[0]))
+    first_col = next(col for col in col_list if row_list[0][0] in col)
+    row_list = sorted(row_list, key=lambda row: first_col.index(row[0]))
 
-recovered_image = cv2.vconcat([cv2.hconcat([image_pieces[piece_index][0] for piece_index in row]) for row in row_list])
-cv2.imshow("recover_image", recovered_image)
-cv2.waitKey(0)
+    recovered_image = cv2.vconcat([cv2.hconcat([image_pieces[piece_index][0] for piece_index in row]) for row in row_list])
+    cv2.imshow("recover_image", recovered_image)
+    cv2.waitKey(0)
+
+
+image = cv2.imread("26e55c44d2824a7891223d7ec125c233.jpg")
+recover_image(image)
